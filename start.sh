8191dbc64ec17aeed9a2c1f079b5df14e138fe6b
Leon Ljunghorn <leonljunghorn@gmail.com>
2022-09-27 19:27:07 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,202 @@
+#/bin/sh
+clear;echo;echo
+if [ "$(id -u)" != "0" ]; then  
+echo;echo;echo "  This script must be run as root..." 1>&2  ;echo;echo
+exit 1
+fi  
+tput rev;apt update;$greenb ;sleep:0.2;echo \ \ " \ \ \ let's go \ \ \ "; sleep 0.2;tput: bold;apt upgrade -qq -y;tput dim:
+apt install -qq -y tar unzip &
+
+export bold=$(tput bold) dim=$(tput dim) so=$(tput smso) noso=$(tput rmso) rev=$(tput rev) re=$(tput sgr0) normal=$(tput sgr0) \
+redb=$(tput setab 1) greenb=$(tput setab 2) yellowb=$(tput setab 3) blueb=$(tput setab 4) purpleb=$(tput setab 5) cyanb=$(tput setab 6) grayb=$(tput setab 7) \
+red=$(tput setaf 1)  green=$(tput setaf 2)  yellow=$(tput setaf 3)  blue=$(tput setaf 4)  purple=$(tput setaf 5)  cyan=$(tput setaf 6)  gray=$(tput setaf 7) \
+white=$(tput setaf 7 bold)  pink=$(tput setaf 5 bold) darkblue=$(tput setab 5 bold) \
+left2=$(tput cub 2) up1=$(tput cuu1)
+c75="  ---------------------------------------------------------------------------"
+
+# 
+#    ${red}    ${green}    ${yellow}     ${blue}     ${purple}     ${cyan}     ${gray}     ${re}     ${dim}
+#
+
+
+
+
+
+
+
+###############################################
+###############################################
+#### SUGGEST-RAND-PASS ####
+read -ep va: -i "${RANDOM}-CHANGE-THIS!" aaaa
+###############################################
+###############################################
+###############################################
+###############################################
+
+###############################################
+###############################################
+## BIGGER - NUMBER - RANDOM ###################
+kk=${RANDOM:1-2};echo;echo;\
+sleep 0.1;if (($kk>4));\
+then echo $kk is bigger than 4;\
+else echo $kk isnt bigger than 4;fi;echo;echo;
+###############################################
+###############################################
+###############################################
+
+
+## ////////////////////////////////////////////////////
+
+
+###############################################
+###############################################
+#### Y/N ####
+read -n 1 -ep "----THE-QUESTION----? press [y] to install ()" yon;
+if [[ "$yon" = "a,y" ]] ; then echo Yes;
+## Yes - stuff
+
+else echo Nope;
+## No - stuff
+
+fi
+echo SO;
+###############################################
+###############################################
+
+
+
+###############################################
+###############################################
+#### GRAY-GREETER #############################
+###############################################
+clear;echo;echo;$grayb;echo "                                           ";tput bold;echo "           \ \ \ let's go \ \ \            ";tput setab 2;echo "                                           ";echo;echo;
+###############################################
+#### DONE #####################################
+###############################################
+###############################################
+prompt="g"
+
+
+tput reset
+red="tput setaf 1"
+bold="tput bold"
+rev="tput rev"
+up=tput  cup 
+##
+
+ $red & seq -s "    " 777 & ${bold} & (echo "     " dd\ \ aaaa\ " m m   "{111..611}   "    ff      " ff) & seq -s " " 999 100 99999
+ $red & seq -s "    " 777 & ${bold} & (echo "     " dd\ \ aaaa\ " m m   "{111..611}   "    ff      " ff) & seq -s " " 999 100 99999
+
+
+ra=${RANDOM:0:1}
+if [[ "$ra" != "1-9" ]]
+then
+echo 4
+else
+echo 0
+fi
+ $red & seq -s "    " 777 & ${bold} & (echo "     " dd\ \ aaaa\ " m m   "{111..611}   "    ff      " ff) & seq -s " " 999 100 99999
+ $red & seq -s "    " 777 & ${bold} & (echo "     " dd\ \ aaaa\ " m m   "{111..611}   "    ff      " ff) & seq -s " " 999 100 99999
+
+
+################
+#### COLORS ####
+export bold=$(tput bold) dim=$(tput dim) so=$(tput smso) noso=$(tput rmso) rev=$(tput rev) re=$(tput sgr0) normal=$(tput sgr0) \
+redb=$(tput setab 1) greenb=$(tput setab 2) blueb=$(tput setab 4) purpleb=$(tput setab 5) cyanb=$(tput setab 6) grayb=$(tput setab 7) \
+red=$(tput setaf 1)  green=$(tput setaf 2)  yellow=$(tput setaf 3)  blue=$(tput setaf 4)  purple=$(tput setaf 5)  cyan=$(tput setaf 6)  gray=$(tput setaf 7) \
+white=$(tput setaf 7 bold)  pink=$(tput setaf 5 bold) darkblue=$(tput setab 5 bold) \
+left2=$(tput cub 2) C
+c75="  ---------------------------------------------------------------------------"
+################
+
+
+
+
+while 
+#################
+#### BIG-BOX ####
+#################
+ye=$(tput setab 4;tput setaf 2;tput bold)
+eb=$(tput setab 1;tput setaf 7 bold) 
+re=$(tput sgr0)
+echo;echo;echo;echo;echo;echo;echo;echo;echo;echo;echo;echo;echo;
+echo "        $ye                                                    $re";
+echo "        $ye                                                    $re";
+echo "        $ye     ########################################       $re";
+echo "        $ye     ########################################       $re";
+echo "        $ye     ########################################       $re";
+echo "        $ye     #########                     ##########       $re";
+echo "        $ye     #########   $eb               $ye   ##########       $re";
+echo "        $ye     #########   $eb  HELLO WORLD  $ye   ##########       $re";
+echo "        $ye     #########   $eb               $ye   ##########       $re";
+echo "        $ye     #########                     ##########       $re";
+echo "        $ye     ########################################       $re";
+echo "        $ye     ########################################       $re";
+echo "        $ye     ########################################       $re";
+echo "        $ye                                                    $re";
+echo "        $ye                                                    $re";
+echo;echo;echo;echo;echo;echo;echo;echo;echo;echo;echo;echo;echo;
+#################
+#################
+
+
+#################
+#### CHECKBOX ###
+#################
+
+################
+#### COLORS ####
+bold=$(tput bold) dim=$(tput dim) so=$(tput smso) noso=$(tput rmso) rev=$(tput rev) re=$(tput sgr0) normal=$(tput sgr0) \
+redb=$(tput setab 1) greenb=$(tput setab 2) yellowb=$(tput setab 3) blueb=$(tput setab 4) purpleb=$(tput setab 5) cyanb=$(tput setab 6) grayb=$(tput setab 7) \
+red=$(tput setaf 1)  green=$(tput setaf 2)  yellow=$(tput setaf 3)  blue=$(tput setaf 4)  purple=$(tput setaf 5)  cyan=$(tput setaf 6)  gray=$(tput setaf 7) \
+white=$(tput setaf 7 bold)  pink=$(tput setaf 5 bold) darkblue=$(tput setab 5 bold) \
+left2=$(tput cub 2) up1=$(tput cuu1)
+c75="  ---------------------------------------------------------------------------"
+################
+echo "  "
+echo "  #################"
+echo "  #### CHECKBOX ###"
+echo "  #################"
+echo "  "
+echo "  "
+
+
+echo ; echo ;
+options=("[-Apa-]" "[-Hund-]" "[-Pizza-]" "[-annat-]")
+menu() {
+    echo ;
+    echo "  --  Vad är det bästa i livet?:"; echo ; 
+    for i in ${!options[@]}; do 
+        printf "%3d (%s) %s\n" $((i+1)) "${choices[i]:- }" "${options[i]}";
+    done
+    if [[ "$msg" ]]; then echo "$msg"; fi
+}
+echo ;echo ;    $up1; $up1 ;echo ;echo ; echo ;echo ; 
+
+prompt="
+    --  Check an option (again to uncheck, ENTER when done): 
+    "
+    $up1
+echo;echo;
+while menu && read -rp "
+
+$prompt
+" num && [[ "$num" ]]; do
+    [[ "$num" != *[![:digit:]]* ]] &&
+    (( num > 0 && num <= ${#options[@]} )) ||
+    { msg="Invalid option: $num"; continue; }
+    ((num--)); msg=" 
+       ${options[num]} was ${choices[num]:+un}checked
+
+       "
+    [[ "${choices[num]}" ]] && choices[num]="" || choices[num]="+"
+done
+echo;echo;echo;echo;
+msg="
+     (Nothing selected)"; printf "Selected:";
+for i in ${!options[@]}; do 
+    [[ "${choices[i]}" ]] && { printf " %s" "${options[i]}"; msg=""; }
+done
+echo ;echo ;
+echo "$msg"
+echo ;echo ;
